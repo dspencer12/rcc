e167fbb766b89b153ab623d459295b58d8353c05
Daniel Spencer <dms305@hku.hk>
2020-12-23 17:08:25 +0000

    Refactor assembly generation before un ops

--- src/compiler/assembly.rs
+++ src/compiler/assembly.rs
@@ -1,32 +1,47 @@
 use super::ast;
 
-pub fn generate(ast: &ast::Node) -> Result<String, &'static str> {
+fn stack_to_string(stack: &mut Vec<String>) -> String {
+    stack.reverse();
+    let mut s = stack.join("\n");
+    s.push('\n');
+    s
+}
+
+fn generate_return(ast: &ast::Node) -> Result<String, &'static str> {
     match ast {
-        ast::Node::Program(node) => match &**node {
-            ast::Node::Function(id, node) => match &**node {
-                ast::Node::Statement(st, node) => match st {
-                    ast::Statement::Return => match &**node {
-                        ast::Node::Expression(expr) => match expr {
-                            ast::Expr::IntLiteral(n) => Ok(format!(
-                                ".globl _{}
-_{}:
-  movl    ${}, %eax
-  ret
-",
-                                id, id, n
-                            )),
-                            _ => Err("Expected int literal"),
-                        },
-                        _ => Err("Expected expression"),
-                    },
-                    _ => Err("Expected return statement"),
-                },
-                _ => Err("Expected statement"),
-            },
-            _ => Err("Expected AST::Function"),
-        },
-        _ => Err("Expected AST::Program"),
+        ast::Node::Expression(expr) => match expr {
+            ast::Expr::IntLiteral(n) => Ok(format!("  movl    ${}, %eax", n)),
+            _ => Err("Expected int literal"),
+        }
+        _ => Err("Expected expression")
     }
+}
+
+fn generate_statement(statement: &ast::Statement, ast: &ast::Node) -> Result<String, &'static str> {
+    let mut code = Vec::new();
+    match statement {
+        ast::Statement::Return => {
+            code.push(String::from("  ret"));
+            code.push(generate_return(ast)?);
+        }
+    }
+    Ok(stack_to_string(&mut code))
+}
+
+pub fn generate(ast: &ast::Node) -> Result<String, &'static str> {
+    let mut code = Vec::new();
+    match ast {
+        ast::Node::Program(node) => code.push(generate(node)?),
+        ast::Node::Function(id, node) => {
+            code.push(generate(node)?);
+            code.push(format!("_{}:", id));
+            code.push(format!(".globl _{}", id));
+        }
+        ast::Node::Statement(st, node) => code.push(generate_statement(st, node)?),
+        ast::Node::Expression(_) => return Err("Unexpected expression"),
+    };
+    code.reverse();
+    Ok(code.join("\n"))
 }
 
 #[cfg(test)]
@@ -40,8 +55,9 @@ mod tests {
                 String::from("foo"),
                 ast::Node::Statement(
                     ast::Statement::Return,
-                    ast::Node::Expression(ast::Expr::IntLiteral(0)).into()
-                ).into()
+                    ast::Node::Expression(ast::Expr::IntLiteral(0)).into(),
+                )
+                .into(),
             )
             .into(),
         );
